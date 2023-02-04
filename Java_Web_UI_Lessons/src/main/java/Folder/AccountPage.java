051eb797da814ed9cd5c5193ff4aa0bd8fce7f75
DariusStone <97939159+DariusStone@users.noreply.github.com>
2023-02-04 10:38:10 +0200

    Add files via upload

---
+++ Java_Web_UI_Lessons/src/main/java/Folder/AccountPage.java
@@ -0,0 +1,24 @@
+package Folder;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountPage extends AbstractPage {
+
+    @FindBy(xpath = ".//span[text()='Sign out']")
+    private WebElement exit;
+
+    public AccountPage(WebDriver driver){
+        super(driver);
+
+    }
+
+
+    public void logOut(){
+        this.exit.click();
+
+    }
+
+}
